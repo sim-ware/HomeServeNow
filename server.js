921e66a508c1351c78722edea51c8c7d56326599
Samir Gossain <samir.gossain@gmail.com>
2020-06-23 19:03:59 +0100

    Completes route for POST createJob

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
 const express = require("express");
 const app = express();
 
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// E X T R A C T &&  // // // // // // // // // // // // // // // // // // // // // // // //
+// R E T U R N : 'db'// // // // // // // // // // // // // // // // // // // // // // // //
+let db;
+let jobsCollection;
+const MongoClient = require("mongodb").MongoClient;
+const connectionString =
+  "mongodb+srv://yoda:skywalker@cluster0-xja5e.mongodb.net/test?retryWrites=true&w=majority";
+MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log("Connected to Database");
+    db = client.db("star-wars-quotes");
+    jobsCollection = db.collection("jobs");
+  }
+);
+console.log("db", db);
+console.log("jobsCollection", jobsCollection);
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
 app.use(express.json());
 
 app.listen(3000, function () {
   console.log("listening on 3000");
 });
 
-app.get("/", (_, res) => {
-  res.send("Hello World");
-});
-
 app.post("/jobs", (req, res) => {
   console.log("req.body", req.body);
-  res.json({ requestBody: req.body });
+  //
+  // // jobsRepository.createJob(req.body);
+  jobsCollection
+    .insertOne(req.body)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => console.error(error));
+  // //
+  //
+  res.json(req.body);
 });
