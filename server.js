0853a12775ca73d10372f662dd4d4bfc8bed1389
Samir Gossain <samir.gossain@gmail.com>
2020-06-26 12:21:56 +0100

    assigns ENV port for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.listen(3000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log("listening on 3000");
 });
 
